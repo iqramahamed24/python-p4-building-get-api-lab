54d49292b0c87ae852b871d22ce1f38775cd5c08
iqramahamed <iqra.mahamed64@gmail.com>
2024-06-30 16:51:40 +0300

    save

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_migrate import migrate
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
